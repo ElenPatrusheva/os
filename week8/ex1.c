b02c204a8261972d79e7f5127602e57978208427
ElenPatrusheva <35638891+ElenPatrusheva@users.noreply.github.com>
2018-10-11 22:49:05 +0300

    Create ex1.c

---
+++ week8/ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+int main() {
+	for (int i=0; i<10; i++){
+		void *ptr=malloc(10*1024*1024);
+		memset(ptr, 0, 10*1024*1024);
+		sleep(1);
+	}
+}
